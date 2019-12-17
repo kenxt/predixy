dabf8e6838c2ec5ef790a5e416c3d4c1efbb89a0
kenxt <43370135+kenxt@users.noreply.github.com>
2019-12-17 10:18:44 +0800

    Add files via upload

---
+++ redis/create-cluster.sh
@@ -0,0 +1,1 @@
+docker run -it --rm --network=redisnet --ip 1.2.3.254 redis redis-cli --cluster create 1.2.3.2:7001 1.2.3.3:7002 1.2.3.4:7003 1.2.3.5:7004 1.2.3.6:7005 1.2.3.7:7006 1.2.3.8:7007 1.2.3.9:7008 --cluster-replicas 1
