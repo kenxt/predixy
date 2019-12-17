7c51b8a40d679de8073d320d692d6e6a8a2d8902
kenxt <43370135+kenxt@users.noreply.github.com>
2019-12-17 10:28:59 +0800

    Delete redis01.sh

--- redis/conf/redis01.sh
+++
@@ -1,1 +0,0 @@
-docker run -d -p 7001:7001  --name redis01 --network=redisnet --ip 1.2.3.2 -v $(pwd)/conf:/etc/redis -v $(pwd)/data:/data --restart=always redis:latest redis-server /etc/redis/redis.conf
